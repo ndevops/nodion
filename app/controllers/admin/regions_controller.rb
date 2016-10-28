26a4d4f176212bb2014d789ad1d25dbf7627939f
Niklas Karoly <niklas@karoly.io>
2016-10-28 19:24:10 +0200

    same for regions

--- app/controllers/admin/regions_controller.rb
+++ app/controllers/admin/regions_controller.rb
@@ -2,4 +2,31 @@ class Admin::RegionsController < Admin::ApplicationController
   def index
     @regions = Region.all
   end
+
+  def new
+    @region = Region.new
+  end
+
+  def create
+    @region = Region.new(region_params)
+
+    if @region.save
+      redirect_to admin_regions_path
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @region = region.find(params[:id])
+    @region.destroy
+
+    redirect_to admin_regions_path
+  end
+
+  private
+
+  def region_params
+    params.require(:region).permit(:name,)
+  end
 end
