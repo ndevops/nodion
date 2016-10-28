f8c96b417ce8426e788a01200bb18f2671948698
Niklas Karoly <niklas@karoly.io>
2016-10-28 19:22:03 +0200

    added the option to add plans, still a wip

--- app/controllers/admin/plans_controller.rb
+++ app/controllers/admin/plans_controller.rb
@@ -2,4 +2,31 @@ class Admin::PlansController < Admin::ApplicationController
   def index
     @plans = Plan.all
   end
+
+  def new
+    @plan = Plan.new
+  end
+
+  def create
+    @plan = Plan.new(plan_params)
+
+    if @plan.save
+      redirect_to admin_plans_path
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @plan = Plan.find(params[:id])
+    @plan.destroy
+
+    redirect_to admin_plans_path
+  end
+
+  private
+
+  def plan_params
+    params.require(:plan).permit(:name, :ram_value, :cpu_value, :storage_value, :storage_type)
+  end
 end
