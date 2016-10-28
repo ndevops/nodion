608008bd70740cbd521138908712e6011f43e075
Niklas Karoly <niklas@karoly.io>
2016-10-28 20:02:36 +0200

    by adding a hypervisor it automatically creates ssh keys now

--- app/actions/hypervisor_action.rb
+++ app/actions/hypervisor_action.rb
@@ -8,9 +8,8 @@ class HypervisorAction
   def test_connection
     begin
       Net::SSH.start("#{@hostname}", "#{@user}", :password => "#{@password}", :timeout => 10) do |ssh|
-        #
-        # TODO: Setup SSH Keys
-        #
+        @key_file = File.read(ENV['HOME']+'/.ssh/id_rsa.pub')
+        ssh.exec!("echo '#{@key_file}' >> /root/.ssh/authorized_keys")
       end
     rescue Timeout::Error
       false
